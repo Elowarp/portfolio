cb41e878d772b2d4d2ef01ef85a9f5b1cd9f55c1
elo <agent.craftien@gmail.com>
2024-03-01 19:11:05 +0100

    Mise à jour des informations

--- vitrine/views.py
+++ vitrine/views.py
@@ -1,26 +1,11 @@
 from .models import Project, PageExemple, Article
-from .forms import ClientForm
 from django.shortcuts import redirect, get_object_or_404, render
-from django.contrib import messages
-from .sendTelegram import send
 from django.http import Http404
 
 # Create your views here.
 def index(request):
     pages = PageExemple.objects.all().order_by("-date")
     articles = Article.objects.all().order_by("-date")
-
-    if request.method == "POST":
-        form = ClientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            send("Tu as reçu une nouvelle demande sur le site bg!")
-            messages.success(request, "Message envoyé !")
-
-        else :
-            messages.error(request, "Quelque chose s'est mal passé avec l'envoie :(")
-            
-        return redirect('/')
 
     return render(request, "index.html", {'pages': pages, 'articles': articles})
 
