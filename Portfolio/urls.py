97777f5db01cd0a4b6f51fd656ad83ed6708dde3
elowarp <elowarp@gmail.com>
2025-07-25 17:37:49 +0100

    Modif index + Blog nouveau design

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('blog/', include("blog.urls")),
     path('admin/', admin.site.urls),
-    path('', include("vitrine.urls")),
+    # path('', include("vitrine.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
