9be0b9fa37e6a058885dd02b8a8ceccf06f6348c
elowarp <elowarp@gmail.com>
2025-12-20 13:35:21 +0000

    Fix building

--- Portfolio/build.py
+++ Portfolio/build.py
@@ -5,7 +5,29 @@ import dotenv
 import os
 import json
 
+import hashlib
+import hmac
+
 dotenv.load_dotenv()
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
 
 # Url to rebuild the project
 @csrf_exempt
@@ -13,16 +35,13 @@ def build(request):
     if request.method != "POST": return HttpResponse(status=404)
 
     BUILDING_KEY = os.getenv("BUILDING_KEY")
-    key = json.loads(request.body)["hook"]["config"]["secret"]
 
-    if (BUILDING_KEY != key): return HttpResponse(status=403)
+    try:
+        verify_signature(body, BUILDING_KEY, request.headers["X-Hub-Signature-256"])
+    except: 
+        print("Signature error !")
+        return HttpResponse(status_code=403)
 
-    print("Building portfolio")
-    process = subprocess.run(["bash", "build.sh"])
-    if (process.returncode != 0):
-        print("Issue while building ! (Error {})".format(process.returncode))
-        return HttpResponse(status=500)
-
-
-    print("Building completed !")
+    print("Launching building !")
+    process = os.system("bash build.sh&") # Launch the build script in the background
     return HttpResponse(status=200)
